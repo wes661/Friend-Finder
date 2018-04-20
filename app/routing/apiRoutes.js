c8c04064757b6c737e6a416265e236365461f364
Wesley Hanson <wrh1600@gmail.com>
2018-04-20 13:59:43 -0700

    working on matching function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,12 +9,42 @@ var getFriends = function showFriends(app){
     });
     app.post("/api/friends", function(req, res){
         var newFriend = req.body
-        console.log(newFriend);
+        var friendScore = newFriend.scores;
+        var scoreArray = [];
+        var match = 0;
+       
+        console.log("friend scores: " + friendScore);
+
+        for(i = 0; i < friends.length; i++){
+           
+            var scoreDiff = 0;
+            for(x = 0; x < friendScore.length; x++){
+                scoreDiff += Math.abs(friends[i].scores[x] - friendScore[x])
+            }
+            scoreArray.push(scoreDiff);
+           
+            // console.log(scoreArray)
+            console.log(friends[i].name + " " + scoreDiff);
+         }
+
+        for(i = 0; i < scoreArray.length; i++){
+            if(scoreArray[i] <= scoreArray[match]){
+                match = i;
+            }
+        } 
+
+        console.log("Closest match: " + friends[match].name);
+
+      
+
+
+
+        
         friends.push(newFriend);
         res.json(newFriend);
-    })
+    });
 
-} 
+}; 
 
 module.exports = getFriends;
 
