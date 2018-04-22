b065f96d9a1f9cb683c05f94a3f7281b22607603
Wesley Hanson <wrh1600@gmail.com>
2018-04-22 00:30:52 -0700

    almost done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,135 @@
 var friendsArr = [
     {
-        'name': 'Wes Hanson',
-        'photo': 'https://wes661.github.io/Custom-Portfolio/assets/images/20161225_111259.jpg',
+        'name': 'Sterling Archer',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/3/3f/Sterling_Archer_Standing_POSE.png/revision/latest/scale-to-width-down/250?cb=20151123090437',
+        'video': 'https://www.youtube.com/embed/9x6KApzgtjU',
         'scores': [
+            5,
+            5,
+            5,
+            4,
+            5,
             2,
+            2,
+            1,
+            4,
+            5
+        ]
+    },
+    {
+        'name': 'Ray Gilletee',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/6/62/Ray_Gillette.png/revision/latest/scale-to-width-down/200?cb=20151013031515',
+        'scores': [
+            4,
+            5,
             4,
             3,
-            5,
-            2,
-            5,
+            3,
             3,
             1,
-            5,
+            2,
+            2,
             2
         ]
     },
     {
-        'name': 'Jane Doe',
-        'photo': 'https://i0.wp.com/sharethelove.co.uk/wp-content/uploads/2015/08/7_p.jpg?fit=600%2C601&ssl=1',
+        'name': 'Lana Kane',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/e/e8/Lana_Kane.png/revision/latest/scale-to-width-down/200?cb=20151013033343',
         'scores': [
-            2,
-            4,
+            3,
             5,
+            5,
+            4,
+            4,
+            2,
             2,
             1,
-            5,
-            4,
-            2,
-            3,
+            1,
             1
         ]
     },
     {
-        'name': 'John Doe',
-        'photo': 'https://pbs.twimg.com/profile_images/1545067031/genericMaleFaceIcon_400x400',
+        'name': 'Pam Poovey',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/b/b9/Pam_Poovey.png/revision/latest?cb=20141013063722',
         'scores': [
             5,
-            5,
-            1,
-            1,
+            2,
             3,
             5,
             4,
+            2,
+            5,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        'name': 'Algernop Krieger',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/b/bb/Algernop_Krieger.png/revision/latest/scale-to-width-down/200?cb=20151013042053',
+        'scores': [
+            2,
+            2,
+            2,
+            3,
+            3,
+            5,
+            3,
+            4,
+            5,
+            3
+        ]
+    },
+    {
+        'name': 'Cheryl Tunt',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/f/fb/Cheryl_Tunt01.png/revision/latest/scale-to-width-down/200?cb=20151013030824',
+        'scores': [
+            4,
+            4,
+            1,
+            2,
+            4,
+            1,
+            3,
+            4,
+            5,
+            3
+        ]
+    },
+    {
+        'name': 'Cyril Figgis',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/d/dc/Cyril_Figgis.png/revision/latest?cb=20141013063526',
+        'scores': [
+            3,
+            3,
+            2,
+            3,
+            3,
             4,
             2,
+            3,
+            1,
             1
+        ]
+    },
+    {
+        'name': 'Malory Archer',
+        'photo': 'https://vignette.wikia.nocookie.net/archer/images/2/26/Malory_Archer.png/revision/latest?cb=20141013063353',
+        'scores': [
+            5,
+            5,
+            3,
+            1,
+            4,
+            3,
+            1,
+            1,
+            2,
+            3
         ]
     }
 ]
 
 module.exports = friendsArr;
+
+
