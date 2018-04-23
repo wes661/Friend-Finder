15a50a33d93d563693424a6e46b1a684fbf61a4f
Wesley Hanson <wrh1600@gmail.com>
2018-04-22 22:15:12 -0700

    working app

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var PORT = 3000;
-
+var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
