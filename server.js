7b7674f2a2d2bf7e88b30332b0a206f3bb2427ae
Wesley Hanson <wrh1600@gmail.com>
2018-04-22 22:04:51 -0700

    everything works, possibly adding other features

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
