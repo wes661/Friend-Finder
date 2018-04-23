c70e78de8d79ac0fdca8d875311b3b88cd55fe08
Wesley Hanson <wrh1600@gmail.com>
2018-04-23 14:52:01 -0700

    changed font

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/static", express.static('public'))
+app.use(express.static('app/public'))
 
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 
